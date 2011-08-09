3057ec48e60a5de103e4531e16dee1f7ae0178fb
Cem Eliguzel <cem@eliguzel.net>
2011-08-09 22:01:11 +0300

    Sinan's changes in actor-design

--- kitapyakala/Yakala.scala
+++ kitapyakala/Yakala.scala
@@ -1,21 +1,35 @@
-package kitapyakala
+package yakala
 
-import kitapyakala.pipelines._
-import kitapyakala.spiders._
+import yakala.pipelines._
+import yakala.spiders._
 import yakala.crawler._
 import yakala.logging._
 
 object Yakala {
+
   def main(args : Array[String]) {
 
     val url = args(0)
-    val logger = new ConsoleLogger()
-    logger.setLogLevel(Logger.LOG_DEBUG)
+
+    val logger    : Logger       = new ConsoleLogger()
+    logger.setLogLevel(Logger.LOG_INFO)
 
     val spiders = List( new PandoraSpider(logger), new ImgeSpider(logger) )
-    val pipeline = new GoogleAppEngineBookDB(logger)
-    val crawler = new Crawler(logger)
 
-    args.foreach(crawler ! _)
+    val pipeline  : ItemPipeline = new GoogleAppEngineBookDB(logger)
+
+    val crawler = new Crawler(logger, pipeline)
+    crawler.start
+
+    args.foreach{ domainName =>
+      spiders.foreach{ spider =>
+        if (domainName == spider.domainName) {
+          spider.start
+          crawler ! (spider, spider.startURL)
+        }
+      }
+    }
   }
+
 }
+
