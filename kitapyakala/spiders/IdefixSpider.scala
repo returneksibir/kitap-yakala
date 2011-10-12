8fc87ed8495e95d44d4c45a8bf824d9bda4f9572
Cem Eliguzel <cem@eliguzel.net>
2011-10-12 22:51:36 +0300

    Improved ISBN pattern match of Idefix spider

--- kitapyakala/spiders/IdefixSpider.scala
+++ kitapyakala/spiders/IdefixSpider.scala
@@ -9,9 +9,9 @@ class IdefixSpider(logger : Logger) extends BaseBookSpider(logger) {
   def startURL()            : String                    = "http://www.idefix.com/kitap/"
   def productPagePattern()  : util.matching.Regex       = """.*/kitap/(.*)/tanim\.asp\?sid=.*""".r
   def pricePath()           : String                    = "b.pricerange"
-  def pricePattern()        : scala.util.matching.Regex = """.{2}(\S+) TL.*""".r
+  def pricePattern()        : util.matching.Regex       = """.{2}(\S+) TL.*""".r
   def isbnPath()            : String                    = "div#tanitimbox.disableSelection"
-  def isbnPattern()         : scala.util.matching.Regex = """.*(?:ISBN : )(\S+).*""".r
+  def isbnPattern()         : util.matching.Regex       = """.*ISBN : (\S+).*""".r
   def storeID()             : Int                       = 2
   
 }
