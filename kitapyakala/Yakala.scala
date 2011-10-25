8a37c5a9042cc9cb26ce3e782acb0de9d47be4b4
Cem Eliguzel <cem@eliguzel.net>
2011-10-25 07:27:31 +0300

    Added a timer to print statistics

--- kitapyakala/Yakala.scala
+++ kitapyakala/Yakala.scala
@@ -4,6 +4,8 @@ import kitapyakala.pipelines._
 import kitapyakala.spiders._
 import yakala.crawler._
 import yakala.logging._
+import java.util.Timer;
+import java.util.TimerTask;
 
 object Yakala {
   def main(args : Array[String]) {
@@ -33,5 +35,24 @@ object Yakala {
         }
       }
     }
+    
+    val task = new TimerTask(){            
+      def run(){
+        val out = new java.io.FileWriter("Stats.txt")
+        out write "\n\n--- Crawler ---"
+        crawler printStats out.write
+        out write "\n\n--- Pipeline ---"
+        pipeline printStats out.write
+        out write "\n\n--- Spiders ---"
+        spiders.foreach{ spider =>
+          spider printStats out.write
+        }
+        out write "\n"
+        out close
+      }
+    };
+     
+    val timer = new Timer();
+    timer.schedule(task, 0, 5000);
   }
 }
