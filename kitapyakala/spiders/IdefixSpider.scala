576c8fb7900b0772a085fc25e2d17d646b343294
Cem Eliguzel <cemeliguzel@gmail.com>
2013-02-22 20:37:48 +0200

    Fixes Idefix spider

--- kitapyakala/spiders/IdefixSpider.scala
+++ kitapyakala/spiders/IdefixSpider.scala
@@ -9,9 +9,9 @@ class IdefixSpider(logger : Logger) extends BaseBookSpider(logger) {
   def startURL()            : String                    = "http://www.idefix.com/kitap/"
   def productPagePattern()  : util.matching.Regex       = """.*/kitap/(.*)/tanim\.asp\?sid=.*""".r
   def followRulePattern()   : util.matching.Regex       = """(.*/kitap/.*)""".r
-  def pricePath()           : String                    = "b.pricerange"
-  def pricePattern()        : util.matching.Regex       = """.{2}(\S+) TL.*""".r
-  def isbnPath()            : String                    = "div#tanitimbox.disableSelection"
+  def pricePath()           : String                    = "table#fiyattbl"
+  def pricePattern()        : util.matching.Regex       = """.*idefix Fiyatı : (\S+) TL .*""".r
+  def isbnPath()            : String                    = "div.tTextPad"
   def isbnPattern()         : util.matching.Regex       = """.*ISBN : (\S+).*""".r
   def storeID()             : Int                       = 2
   
